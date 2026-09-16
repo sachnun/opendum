8c78bf5282797a963be14325fd200e4ed788ed14
sachnun <sachnun12@gmail.com>
2026-09-16 16:48:54 +0000

    refactor: keep shell mounted

---
+++ apps/dashboard/app/components/AuthGate.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { useSession } from "../../lib/auth-client";
+
+const { data: session } = await useSession(useFetch);
+const isAuthenticated = computed(() => Boolean(session.value?.user));
+</script>
+
+<template>
+  <LoginScreen v-if="!isAuthenticated" />
+  <NuxtLayout v-else>
+    <slot />
+  </NuxtLayout>
+</template>
